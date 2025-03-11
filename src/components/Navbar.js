ec6a88047c88c3f96619c6ebfe53031103d35381
himanshuchhabran <chhabrahimanshu4@gmail.com>
2025-03-11 20:35:29 +0530

    Navbar created

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,54 @@
 import React from 'react'
+import {Link} from "react-router-dom"
+import logo from "../assets/examdin_logo.png"
+const Navbar = (props) => {
 
-const Navbar = () => {
+  let isLoggedIn = props.isLoggedIn;
+  let setLoggedIn = props.setLoggedIn;
   return (
-    <div>Navbar</div>
+    <div className="flex gap-3 justify-evenly">
+    
+      <Link to="/">
+        <img src={logo} alt="logo" width={50} height = {32} loading ="lazy"/>
+      </Link>
+      
+      <nav >
+        <ul className="flex gap-3">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/">About</Link>
+          </li>
+          <li>
+            <Link to="/">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <div className="flex gap-3"> 
+        {!isLoggedIn && <Link to="/login">
+          <button>Login</button>
+        </Link>
+        }
+        { !isLoggedIn &&
+        <Link to="/signup">
+          <button>Sign Up</button>
+        </Link>
+        }
+        { isLoggedIn&&
+        <Link to="/">
+          <button>Logout</button>
+        </Link>
+        }
+        {
+          isLoggedIn &&
+        <Link to="/dashboard">
+          <button>Dashboard</button>
+        </Link>
+        }
+      </div>
+    </div>
   )
 }
 
