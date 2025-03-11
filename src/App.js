6dc7db2dbd8f054328263de92ade557ab067cbc4
himanshuchhabran <chhabrahimanshu4@gmail.com>
2025-03-11 20:06:10 +0530

    routes created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
 
 
 function App() {
   return (
     <div >
-    
+    <Navbar/>
+    <Routes>
+    <Route path="/" element={<Home/>}/>
+      <Route path="/login" element={<Login/>}></Route>
+      <Route path="/signup" element={<Signup/>}/>
+      <Route path="/dashboard" element={<Dashboard/>}/> 
+    </Routes>
     </div>
   );
 }
